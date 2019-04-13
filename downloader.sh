07172bb29dcfaf6365b5eb5bccdd61f145874169
supun-kanda <supunkandambige9@gmail.com>
2019-04-13 20:07:02 +0530

    updated downloader

--- downloader.sh
+++ downloader.sh
@@ -1,6 +1,11 @@
 file=$1
+path=$2
 if [ ! -z $file ] && [ ! -e $file ]; then
     echo "Given File not exists. Please give a valid 1 file as an argument"
+    exit
+fi
+if [ ! -z $path ] && [ ! -d $path ]; then
+    echo "Given path not exists. Please give a valid path as an argument"
     exit
 fi
 
@@ -9,7 +14,7 @@ expand=`cat $file`
 echo "Start!"
 for p in $expand; do
     url="https://www.youtube.com/watch?v=${p}"
-    echo $url 
-    youtube-dl -f best $url
+    echo "$url will be downloaded to $path" 
+    youtube-dl -f best -o $path/'%(title)s.%(ext)s' $url
 done
 echo "Finish"
